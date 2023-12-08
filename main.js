b208e4796d235b0d6472ed7f881d74e106757d3c
hussein khalil <husseinkhalil420@gmail.com>
2023-12-08 19:57:41 +0200

    fixed some issues

---
+++ main.js
@@ -0,0 +1,30 @@
+projects = document.querySelectorAll("#projects .carousel-item")
+background = document.getElementById("projects-container")
+next = document.querySelector(".projects .carousel-control-next")
+prev = document.querySelector(".projects .carousel-control-prev")
+next.addEventListener("click", () => {
+    projects.forEach(project => {
+        if(project.classList.contains('active') && project.classList.contains('first')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/1.jpeg')";
+        }else if(project.classList.contains('active') && project.classList.contains('mokhber')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mokhber.png')";
+        }else if(project.classList.contains('active') && project.classList.contains('mawared')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mawared.png')";
+        }else if(project.classList.contains('active') && project.classList.contains('podcast')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/podcast.png')";
+        }
+    });
+})
+prev.addEventListener("click", () => {
+    projects.forEach(project => {
+        if(project.classList.contains('active') && project.classList.contains('first')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/1.jpeg')";
+        }else if(project.classList.contains('active') && project.classList.contains('mokhber')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mokhber.png')";
+        }else if(project.classList.contains('active') && project.classList.contains('mawared')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mawared.png')";
+        }else if(project.classList.contains('active') && project.classList.contains('podcast')){
+            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/podcast.png')";
+        }
+    });
+})
