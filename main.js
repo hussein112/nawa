fe574048208a2df7291a04baea19d13f768f4c1f
hussein khalil <husseinkhalil420@gmail.com>
2023-12-09 21:55:36 +0200

    home page review fixes done

--- main.js
+++ main.js
@@ -2,20 +2,8 @@ projects = document.querySelectorAll("#projects .carousel-item")
 background = document.getElementById("projects-container")
 next = document.querySelector(".projects .carousel-control-next")
 prev = document.querySelector(".projects .carousel-control-prev")
-next.addEventListener("click", () => {
-    projects.forEach(project => {
-        if(project.classList.contains('active') && project.classList.contains('first')){
-            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/1.jpeg')";
-        }else if(project.classList.contains('active') && project.classList.contains('mokhber')){
-            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mokhber.png')";
-        }else if(project.classList.contains('active') && project.classList.contains('mawared')){
-            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/mawared.png')";
-        }else if(project.classList.contains('active') && project.classList.contains('podcast')){
-            background.style.backgroundImage  = "url('assets/images/projects/backgrounds/podcast.png')";
-        }
-    });
-})
-prev.addEventListener("click", () => {
+const myCarousel = document.getElementById('projects')
+myCarousel.addEventListener('slide.bs.carousel', event => {
     projects.forEach(project => {
         if(project.classList.contains('active') && project.classList.contains('first')){
             background.style.backgroundImage  = "url('assets/images/projects/backgrounds/1.jpeg')";
